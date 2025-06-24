1fb839e83a99bd7c2cfd17ca16c577b1e8613733
Alexander Bass <alexbass@csus.edu>
2025-06-24 00:03:06 -0700

    fix GitHub Pages deployment and config

--- svelte.config.js
+++ svelte.config.js
@@ -7,16 +7,12 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// Default options for GitHub Pages
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
-		}
+		})
 	}
 };
 
